6edc37e6114af5310ea9afe6b6fe5ab29e9d46ca
Mohit kumar <async.mohitt@gmail.com>
2025-04-13 23:29:03 +0530

    Add Hero and Navbar components
    - Created a new Hero component with a simple structure.
    - Implemented a Navbar component with a logo and navigation options.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='mx-auto bg-red-200 flex items-center justify-between px-6 py-3 fixed top-0 z-20 w-full borde-gray-100 shadow-xl lg:px-[180px] '>
+        {/* here is the logo */}
+            <h1 className='font-[monospace]'>GrossMartt.</h1>
+        {/* here is the Nav options section */}
+        <nav className='flex gap-5'>
+         <div className='text-sm font-semibold md:flex items-center gap-7 hidden'>
+            <h4>Home</h4>
+            <h4>Shop</h4>
+            <h4>About</h4>
+            <h4>Contacts</h4>
+         </div>
+        </nav>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
