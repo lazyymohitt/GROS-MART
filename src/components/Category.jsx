e4e369582a1ab2bae501606b40b72c89f40d769d
Mohit kumar <async.mohitt@gmail.com>
2025-04-17 23:15:43 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,50 +1,56 @@
-import React from 'react'
-import CategoryCard from './CategoryCard'
+import React from "react";
+import CategoryCard from "./CategoryCard";
+import Fruits from "../assets/Fruits.webp";
+import Vegetables from "../assets/Vegetables.jpg";
+import Meat from "../assets/Meat.jpg";
+import Bakery from "../assets/Bakery.webp"
+import Dairy from "../assets/Dairy.png"
+import Beverages from "../assets/Beverages.jpg"
 
-  const categoryItems = [
-    {
-      name:"Fruits",
-      image: Fruits
-    },
-    {
-      name:"Vegetables",
-      image:vegetables
-    }
-  ,  {
-    name:"Beverages",
-    image:beverages
-  },{
-    name:"Bakery",
-    image:bakery,
-
-  },{
-    name:"Dairy",
-    image:dairy
-  },{
-    name:"Meat"
-  ,
-  image:meat
-  }]
+const categoryItems = [
+  {
+    name: "Fruits",
+    image: Fruits,
+  },
+  {
+    name: "Vegetables",
+    image: Vegetables,
+  },
+  {
+    name: "Beverages",
+    image: Beverages,
+  },
+  {
+    name: "Bakery",
+    image: Bakery,
+  },
+  {
+    name: "Dairy",
+    image: Dairy,
+  },
+  {
+    name: "Meat",
+    image: Meat,
+  },
+];
 
 const Category = () => {
   return (
-    <section className='py-12 bg-gray-50 px-6 md:px-0'>
-    <div className='max-w-6xl mx-auto '>
-    <h2 className='text-3xl font-semibold text-center mb-8'>
- Shop By Category 
-    </h2>
- <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4'>
-{
-categoryItems.map((item,index)=>{
-return <CategoryCard
-name={item.name} key={index}  image={item.image}/>
-})
-}
- </div>
-    </div>
-
+    <section className="py-12 bg-gray-50 px-6 md:px-0">
+      <div className="max-w-5xl mx-auto ">
+        <h2 className="text-3xl font-semibold text-center mb-8">
+          Shop By Category
+        </h2>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
+          {categoryItems.map((item, index) => {
+            return (
+              <CategoryCard name={item.name} key={index} image={item.image} />
+            );
+          })}
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const CategoryCard = () => {
+const CategoryCard = ({image, name}) => {
   return (
-    <div>heyyyy</div>
+    <div className='overflow-hidden transition-all ease-linear  hover:shadow-2xl border border-gray-200 rounded-xl'>
+        <div className=''>
+        <div className='aspect-square relative bg-gray-200'>
+            <img src={image} className='object-cover w-full h-full' alt={image} />
+        </div>
+        <div className='p-2 text-center'>
+                <h3 className='font-medium tracking-wide'>{name}</h3>
+        </div>
+        </div>
+    </div>
   )
 }
 
