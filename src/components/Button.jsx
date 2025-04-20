e334ac231fb356851f6d20882e0aad32591dd34d
Mohit kumar <async.mohitt@gmail.com>
2025-04-21 00:45:19 +0530

    feat: implement FeaturedProducts component and clean up Button spacing

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,7 +10,6 @@ const Button = ({ id, title, rightIcon, leftIcon, containerClass }) => {
       )}
     >
       {leftIcon}
-
       <span className="relative inline-flex overflow-hidden text-xs uppercase">
         <div className="translate-y-0 skew-y-0 transition duration-500 group-hover:translate-y-[-160%] group-hover:skew-y-12">
           {title}
@@ -18,8 +17,7 @@ const Button = ({ id, title, rightIcon, leftIcon, containerClass }) => {
         <div className="absolute translate-y-[164%] skew-y-12 transition duration-500 group-hover:translate-y-0 group-hover:skew-y-0">
           {title}
         </div>
-      </span>
-
+      </span> 
       {rightIcon}
     </button>
   );
